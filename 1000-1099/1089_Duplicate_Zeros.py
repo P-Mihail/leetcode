f9b0775f3b66cfe90409d1f46b3f596f59b5adb9
Mihail Parushkin <mihail.parushkin@yandex.ru>
2021-06-18 23:51:04 +0700

    1089 Duplicate Zeros

---
+++ 1000-1099/1089_Duplicate_Zeros.py
@@ -0,0 +1,30 @@
+# Given a fixed length array arr of integers, duplicate each occurrence of zero,
+# shifting the remaining elements to the right.
+#
+# Note that elements beyond the length of the original array are not written.
+#
+# Do the above modifications to the input array in place, do not return anything from your function.
+#
+# Example 1:
+# Input: [1,0,2,3,0,4,5,0]
+# Output: null
+# Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+#
+# Example 2:
+# Input: [1,2,3]
+# Output: null
+# Explanation: After calling your function, the input array is modified to: [1,2,3]
+#
+# Constraints:
+# 1 <= arr.length <= 10000
+# 0 <= arr[i] <= 9
+
+
+from typing import List
+
+
+class Solution(object):
+    def duplicateZeros(self, arr: List[int]) -> None:
+        tmp = "".join(str(x) for x in arr).replace("0", "00")
+        for i in range(len(arr)):
+            arr[i] = int(tmp[i])
