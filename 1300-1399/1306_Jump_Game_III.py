7db5d9cc38e37aef88a4b10d3879c5ef350a85cd
Mihail Parushkin <mihail.parushkin@yandex.ru>
2021-06-26 17:16:33 +0700

    1306. Jump Game III

---
+++ 1300-1399/1306_Jump_Game_III.py
@@ -0,0 +1,65 @@
+# Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at
+# index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+#
+# Notice that you can not jump outside of the array at any time.
+#
+#
+#
+# Example 1:
+# Input: arr = [4,2,3,0,3,1,2], start = 5
+# Output: true
+# Explanation:
+# All possible ways to reach at index 3 with value 0 are:
+# index 5 -> index 4 -> index 1 -> index 3
+# index 5 -> index 6 -> index 4 -> index 1 -> index 3
+#
+# Example 2:
+# Input: arr = [4,2,3,0,3,1,2], start = 0
+# Output: true
+# Explanation:
+# One possible way to reach at index 3 with value 0 is:
+# index 0 -> index 4 -> index 1 -> index 3
+#
+# Example 3:
+# Input: arr = [3,0,2,1,2], start = 2
+# Output: false
+# Explanation: There is no way to reach at index 1 with value 0.
+#
+# Constraints:
+# 1 <= arr.length <= 5 * 10^4
+# 0 <= arr[i] < arr.length
+# 0 <= start < arr.length
+
+
+import collections
+from typing import List
+
+
+class Solution(object):
+    def canReach(self, arr: List[int], start: int) -> bool:
+        # BFS
+        # time complexity O(n)
+        # space complexity O(n)
+        if arr[start] == 0:
+            return True
+
+        stack = collections.deque([start])
+        visited = {start}
+
+        while len(stack) > 0:
+            point = stack.popleft()
+            nlp, nrp = point - arr[point], point + arr[point]
+
+            if nlp >= 0 and nlp not in visited:
+                if (arr[nlp]) == 0:
+                    return True
+                visited.add(nlp)
+                stack.append(nlp)
+
+            if nrp < len(arr) and nrp not in visited:
+                if (arr[nrp]) == 0:
+                    return True
+                visited.add(nrp)
+                stack.append(nrp)
+
+        return False
