6b025678c5faa42e7d8f407865b415a3c33a0fb3
Mihail Parushkin <mihail.parushkin@yandex.ru>
2021-07-20 13:21:32 +0700

    1742. Maximum Number of Balls in a Box

---
+++ 1700-1799/1742_Maximum_Number_of_Balls_in_a_Box.py
@@ -0,0 +1,70 @@
+# You are working in a ball factory where you have n balls numbered from lowLimit up to highLimit inclusive (i.e., n ==
+# highLimit - lowLimit + 1), and an infinite number of boxes numbered from 1 to infinity.
+#
+# Your job at this factory is to put each ball in the box with a number equal to the sum of digits of the ball's number.
+# For example, the ball number 321 will be put in the box number 3 + 2 + 1 = 6 and the ball number 10 will be put in the
+# box number 1 + 0 = 1.
+#
+# Given two integers lowLimit and highLimit, return the number of balls in the box with the most balls.
+#
+#
+# Example 1:
+# Input: lowLimit = 1, highLimit = 10
+# Output: 2
+# Explanation:
+# Box Number:  1 2 3 4 5 6 7 8 9 10 11 ...
+# Ball Count:  2 1 1 1 1 1 1 1 1 0  0  ...
+# Box 1 has the most number of balls with 2 balls.
+#
+# Example 2:
+# Input: lowLimit = 5, highLimit = 15
+# Output: 2
+# Explanation:
+# Box Number:  1 2 3 4 5 6 7 8 9 10 11 ...
+# Ball Count:  1 1 1 1 2 2 1 1 1 0  0  ...
+# Boxes 5 and 6 have the most number of balls with 2 balls in each.
+#
+# Example 3:
+# Input: lowLimit = 19, highLimit = 28
+# Output: 2
+# Explanation:
+# Box Number:  1 2 3 4 5 6 7 8 9 10 11 12 ...
+# Ball Count:  0 1 1 1 1 1 1 1 1 2  0  0  ...
+# Box 10 has the most number of balls with 2 balls.
+#
+#
+# Constraints:
+# 1 <= lowLimit <= highLimit <= 10^5
+
+
+from typing import Dict
+from collections import defaultdict
+
+
+class Solution:
+    # def countBalls(self, lowLimit: int, highLimit: int) -> int:
+    #     # time complecity O(n log m)
+    #     # space complexity O(log n)
+    #     c: Dict[int, int] = defaultdict(int)
+    #
+    #     for n in range(lowLimit, highLimit + 1):
+    #         c[sum(int(x) for x in str(n))] += 1
+    #
+    #     return max(c.values())
+
+    def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        # time complecity O(n log m)
+        # space complexity O(log n)
+        def sumdig(n: int) -> int:
+            ans = 0
+            while n > 0:
+                n, m = divmod(n, 10)
+                ans += m
+            return ans
+
+        c: Dict[int, int] = defaultdict(int)
+
+        for n in range(lowLimit, highLimit + 1):
+            c[sumdig(n)] += 1
+
+        return max(c.values())
