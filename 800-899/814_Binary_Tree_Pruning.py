3cb3344668bd3a674032fc0430975c28f859d156
Mihail Parushkin <mihail.parushkin@yandex.ru>
2021-07-23 16:26:53 +0700

    814. Binary Tree Pruning recursive

--- 800-899/814_Binary_Tree_Pruning.py
+++ 800-899/814_Binary_Tree_Pruning.py
@@ -54,23 +54,38 @@ class TreeNode:
 
 
 class Solution:
+    # def pruneTree(self, root: TreeNode) -> Optional[TreeNode]:
+    #     # iterative
+    #     # time complexity O(n)
+    #     # space complexity O(n)
+    #     stack = [(root, False)]
+    #     toprune = set()
+    #
+    #     while stack:
+    #         node, visited = stack.pop()
+    #         if node:
+    #             if visited:
+    #                 if node.left in toprune:
+    #                     node.left = None
+    #                 if node.right in toprune:
+    #                     node.right = None
+    #                 if node.val == 0 and node.left is None and node.right is None:
+    #                     toprune.add(node)
+    #             else:
+    #                 stack.extend(
+    #                     [(node, True), (node.left, False), (node.right, False)]
+    #                 )
+    #
+    #     return root if root not in toprune else None
+
     def pruneTree(self, root: TreeNode) -> Optional[TreeNode]:
-        stack = [(root, False)]
-        toprune = set()
+        # recursive
+        # time complexity O(n)
+        # space complexity O(n)
+        if root is None:
+            return None
 
-        while stack:
-            node, visited = stack.pop()
-            if node:
-                if visited:
-                    if node.left in toprune:
-                        node.left = None
-                    if node.right in toprune:
-                        node.right = None
-                    if node.val == 0 and node.left is None and node.right is None:
-                        toprune.add(node)
-                else:
-                    stack.extend(
-                        [(node, True), (node.left, False), (node.right, False)]
-                    )
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
 
-        return root if root not in toprune else None
+        return root if root.val != 0 or root.left or root.right else None
