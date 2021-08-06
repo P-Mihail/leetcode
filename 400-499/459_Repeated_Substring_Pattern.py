162dd81ad7b49ad76ab6490e1e10b56c7e8dce2e
Mihail Parushkin <mihail.parushkin@yandex.ru>
2021-08-06 12:19:05 +0700

    459. Repeated Substring Pattern

---
+++ 400-499/459_Repeated_Substring_Pattern.py
@@ -0,0 +1,39 @@
+# Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the
+# substring together.
+#
+#
+#
+# Example 1:
+# Input: s = "abab"
+# Output: true
+# Explanation: It is the substring "ab" twice.
+#
+# Example 2:
+# Input: s = "aba"
+# Output: false
+#
+# Example 3:
+# Input: s = "abcabcabcabc"
+# Output: true
+# Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+#
+#
+# Constraints:
+# 1 <= s.length <= 10^4
+# s consists of lowercase English letters.
+
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        p = 0
+
+        while p < len(s) // 2:
+            if (
+                len(s) % (p + 1) == 0
+                and s[-1] == s[p]
+                and s[: p + 1] * (len(s) // (p + 1)) == s
+            ):
+                return True
+            p += 1
+
+        return False
