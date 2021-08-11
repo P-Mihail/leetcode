d464dea8c88a97dd8d0abdaa3bfc8c73cf70bc43
Mihail Parushkin <mihail.parushkin@yandex.ru>
2021-08-12 01:09:11 +0700

    434. Number of Segments in a String

---
+++ 400-499/434_Number_of_Segments_in_a_String.py
@@ -0,0 +1,42 @@
+# You are given a string s, return the number of segments in the string. 
+# A segment is defined to be a contiguous sequence of non-space characters.
+# 
+# Example 1:
+# Input: s = "Hello, my name is John"
+# Output: 5
+# Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+# 
+# Example 2:
+# Input: s = "Hello"
+# Output: 1
+# 
+# Example 3:
+# Input: s = "love live! mu'sic forever"
+# Output: 4
+# 
+# Example 4:
+# Input: s = ""
+# Output: 0
+# 
+# Constraints:
+# 0 <= s.length <= 300
+# s consists of lower-case and upper-case English letters, digits or one of the following characters "!@#$%^&*()_+-=',.:".
+# The only space character in s is ' '.
+
+
+class Solution:
+    # def countSegments(self, s: str) -> int:
+    #     # time complexity O(n)
+    #     # space complexity O(n)
+    #     return len(s.split())
+
+    def countSegments(self, s: str) -> int:
+        # time complexity O(n)
+        # space complexity O(1)
+        c = 0
+
+        for i in range(len(s)):
+            if (i == 0 or s[i-1] == ' ') and s[i] != ' ':
+                c += 1
+
+        return c
