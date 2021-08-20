bd24ed0e56239486672bf680030105225f751729
Mihail Parushkin <mihail.parushkin@yandex.ru>
2021-08-21 00:41:28 +0700

    709. To Lower Case

---
+++ 700-799/709_To_Lower_Case.py
@@ -0,0 +1,26 @@
+# Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+# 
+# Example 1:
+# Input: s = "Hello"
+# Output: "hello"
+# 
+# Example 2:
+# Input: s = "here"
+# Output: "here"
+# 
+# Example 3:
+# Input: s = "LOVELY"
+# Output: "lovely"
+# 
+# Constraints:
+# 1 <= s.length <= 100
+# s consists of printable ASCII characters.
+
+class Solution:
+    # def toLowerCase(self, s: str) -> str:
+    #     return s.lower()
+
+
+    def toLowerCase(self, s: str) -> str:
+        return ''.join([chr(ord(char)+32) if 65 <= ord(char) <= 90 else char for char in s])
+
